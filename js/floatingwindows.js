94b02f190ee076ca8618e913fba241838f029b78
FailedEx <thereisanimposteramongus@gmail.com>
2024-05-23 22:21:45 +1200

    slight modularization

---
+++ js/floatingwindows.js
@@ -0,0 +1,94 @@
+var zcount = 1;
+
+function childSet (div, attribute, value) {
+    div.childNodes.forEach(element => {
+        if (element.style){
+            element.style[attribute] = value;
+        }
+    });
+}
+
+export function makefloat (div, position) {
+
+    div.style.position = "fixed";
+    div.style.top = (position.top-10+window.scrollY).toString()+"px";
+    div.style.left = (position.left-10+window.scrollX).toString()+"px";
+    div.style.height = (position.height-24).toString()+"px";
+    div.style.width = (position.width-24).toString()+"px";
+    div.style.userSelect = "none";
+    childSet(div, "pointer-events", "none");
+    div.style.zIndex = 1;
+
+    if (div.getAttribute('listener') === 'true') 
+        return;
+
+    div.setAttribute('listener', 'true');
+
+    let offset = [div.offsetLeft, div.offsetTop];;
+    let held = false;
+    let rheld = false;
+
+    div.addEventListener('mousedown', (e) => {
+        if (div.style.position !== "fixed") 
+            return;
+
+        if (e.button === 0) {
+            held = true;
+            div.setAttribute("held", "true");
+            offset = [
+                div.offsetLeft - e.clientX, 
+                div.offsetTop - e.clientY, 
+            ];
+        } else if (e.button === 2) {
+            rheld = true;
+            div.setAttribute("held", "true");
+            offset = [
+                div.offsetWidth - e.clientX, 
+                div.offsetHeight - e.clientY, 
+            ];
+        }
+
+        div.style.zIndex = zcount;
+        zcount++;
+    }, true);
+
+    const stopHold = () => {
+        if (div.style.position !== "fixed") 
+            return;
+
+        held = false;
+        rheld = false;
+        div.setAttribute("held", "false");
+    }
+
+    div.addEventListener('mouseup', stopHold, true);
+    div.addEventListener('mouseout', stopHold, true);
+
+    div.addEventListener('mousemove', (e) => {
+        if (div.style.position !== "fixed") 
+            return;
+
+        e.preventDefault();
+        if (held) {
+            div.style.left = (e.clientX + offset[0]-10+window.scrollX).toString()+'px';
+            div.style.top = (e.clientY + offset[1]-10+window.scrollY).toString()+'px';
+        } else if (rheld) {
+            // includes the border for some reason
+            div.style.width = (e.clientX + offset[0]-24+window.scrollX).toString()+'px';
+            div.style.height = (e.clientY + offset[1]-24+window.scrollY).toString()+'px';
+        }
+    }, true);
+}
+
+export function unmakefloat (div) {
+    div.style.position = "relative";
+    div.style.zIndex = 1;
+    div.style.top = "";
+    div.style.left = "";
+    div.style.height = "";
+    div.style.width = "";
+    div.style.userSelect = "";
+    childSet(div, "pointer-events", "");
+    zcount = 1;
+}
+
---
+++ js/gravity.js
@@ -0,0 +1,39 @@
+
+const friction = 0.88;
+const gravity = 3;
+
+export function makefall (div, oldpos) {
+    let pos = div.getBoundingClientRect();
+
+    const velocity = [(pos.left-oldpos.left)*friction, (pos.top-oldpos.top)+gravity];
+
+    if (div.getAttribute("held") === "true") 
+        // can't really explain it but this works
+        velocity[1] -= gravity;
+
+    if (pos.left < window.scrollX+50 && velocity[0] < 0) {
+        div.style.left = (window.scrollX).toString()+"px";
+        velocity[0] *= -1;
+    }
+
+    if (pos.left + pos.width > window.innerWidth+window.scrollX && velocity[0] > 0) {
+        div.style.left = (window.innerWidth+window.scrollX-pos.width).toString()+"px";
+        velocity[0] *= -1;
+    }
+
+    if (pos.top+pos.height > window.innerHeight && velocity[1] > 0) {
+        if (Math.abs(velocity[1]-gravity) > 5) {
+            div.style.top = (window.innerHeight+window.scrollY-pos.height).toString()+"px";
+            velocity[1] *= 0.5;
+            velocity[1] *= -1;
+        } else {
+            velocity[1] = 0;
+        }
+    }
+
+    if (Math.max(Math.abs(velocity[0]), Math.abs(velocity[1])) > 1) {
+        div.style.top = (pos.top+window.scrollY+velocity[1]-10).toString()+"px";
+        div.style.left = (pos.left+window.scrollX+velocity[0]-10).toString()+"px";
+    }
+    return pos;
+}
