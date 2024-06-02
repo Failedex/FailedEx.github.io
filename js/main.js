0b3314f40c451b0bc2080b0ab3dbb64c8b17dbd8
FailedEx <thereisanimposteramongus@gmail.com>
2024-06-02 23:22:58 +1200

    fixed some boxes

--- js/main.js
+++ js/main.js
@@ -236,3 +236,42 @@ class ProjectBox extends HTMLElement {
 }
 
 customElements.define("project-item", ProjectBox);
+
+// let workspace = document.getElementsByClassName("workspace");
+// if (workspace.length >= 1) {
+//     workspace = workspace[0];
+//     const sideinfo = document.createElement("div");
+//     sideinfo.style.flexGrow = 1;
+//     sideinfo.innerHTML = `
+//         <article class="centered">
+//             <img style="pointer-events: none;" src="/assets/fieshidle.gif" alt="profile picture" style="width: 100px; height: 100px;">
+//             <h2>Hello I'm Failed!</h2>
+//             <ul>
+//                 <li>Discord: failed.sh</li>
+//                 <li>Github: <a href="https://github.com/Failedex" draggable="false">FailedExperiment</a></li>
+//             </ul>
+//         </article>
+//         <article class="centered">
+//             <h2>Navigation</h2>
+//             <div id="navwindow">
+//                 <a href="/" class="button naventry current">
+//                     <i class="material-symbols-outlined">home</i>
+//                     <p>Home</p>
+//                 </a>
+//                 <a href="/blogs/index.html" class="button naventry">
+//                     <i class="material-symbols-outlined">article</i>
+//                     <p>Blogs</p>
+//                 </a>
+//                 <a href="/experiments/index.html" class="button naventry">
+//                     <i class="material-symbols-outlined">experiment</i>
+//                     <p>Experiments</p>
+//                 </a>
+//                 <a href="/blackjack/index.html" class="button naventry">
+//                     <i class="material-symbols-outlined">playing_cards</i>
+//                     <p>Blackjack</p>
+//                 </a>
+//             </div>
+//         </article>
+//         `;
+//     workspace.appendChild(sideinfo);
+// }
